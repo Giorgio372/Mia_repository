50a1c5ea89bf462465fe420fad296afaad19a529
Giorgio372 <giorgio.bruzzone@gmail.com>
2023-05-22 09:09:57 +0200

    esercizio creato da me

--- es_creato_da_me/CostruzioneChitarra.java
+++ es_creato_da_me/CostruzioneChitarra.java
@@ -1,4 +1,4 @@
-package es_creato_da_me;
+package java1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class CostruzioneChitarra {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
         List<String> componenti = new ArrayList<String>();
         List<String> chitarra = new ArrayList<String>();
         componenti.add("Chiavette"); //il metodo add della classe aggiunge un elemento
@@ -15,82 +15,86 @@ public class CostruzioneChitarra {
         componenti.add("Potenziometri");
         componenti.add("Manico");
         componenti.add("Corde");
-
-        Scanner input = new Scanner(System.in);
         int i = 0;
-        while (i == 1){
+        System.out.print(i);
+    while (i == 1){
             i = 0;
-            System.out.println("Inserisci il prossimo pezzo da assemblare: ");
+            System.out.print("Inserisci il prossimo pezzo da assemblare partendo dal corpo: ");
             String input1 = input.next();
             if (input1 == "Chiavette"){
-                System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
+                System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
                 chitarra.add("Chiavette");
                 i = 1;
             } else {
                 System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
             }
         }
-        while (i == 1){
+        i = 0;
+    while (i == 1){
             i = 0;
-            System.out.println("Inserisci il prossimo pezzo da assemblare: ");
+            System.out.print("Inserisci il prossimo pezzo da assemblare: ");
             String input2 = input.next();
             if (input2 == "Ponte"){
-            System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
+            System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
             chitarra.add("Ponte");
             i = 1;
             } else {
-            System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
+            System.out.print("Peccato! Hai scelto il pezzo sbagliato da assemblare");
             }
             }
-        while (i == 1) {
-                System.out.println("Inserisci il prossimo pezzo da assemblare: ");
+            i = 0;
+    while (i == 1) {
+                System.out.print("Inserisci il prossimo pezzo da assemblare: ");
                 String input3 = input.next();
                 if (input3 == "Pickups"){
-                System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
+                System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
                 chitarra.add("Pickups");
                 i = 0;
                 } else {
-                System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
+                System.out.print("Peccato! Hai scelto il pezzo sbagliato da assemblare");
                 }
             }
-    
-            while (i == 1) {
+            i = 0;
+    while (i == 1) {
                 i = 0;
-                System.out.println("Inserisci il prossimo pezzo da assemblare: ");
+                System.out.print("Inserisci il prossimo pezzo da assemblare: ");
                 String input4 = input.next();
                 if (input4 == "Potenziometri"){
-                System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
+                System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
                 chitarra.add("Potenziometri");
                 i = 1;
                 } else {
-                System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
+                System.out.print("Peccato! Hai scelto il pezzo sbagliato da assemblare");
                 }
             }
-            while (i == 1) {
+            i = 0;
+    while (i == 1) {
                 i = 0;
-                    System.out.println("Inserisci il prossimo pezzo da assemblare: ");
+                    System.out.print("Inserisci il prossimo pezzo da assemblare: ");
                     String input5 = input.next();
                     if (input5 == "Manico"){
-                    System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
+                    System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
                     chitarra.add("Manico");
                     i = 1;
                     } else {
-                    System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
+                    System.out.print("Peccato! Hai scelto il pezzo sbagliato da assemblare");
                     }
                 }
-                while (i == 1) {
-                    i = 0;
-                        System.out.println("Inserisci il prossimo pezzo da assemblare: ");
-                        String componente = input.next();
-                        if (componente == "Corde"){
-                        System.out.println("Bravo hai scelto il pezzo giusto da assemblare");
-                        chitarra.add("Corde");
-                        i = 1;
-                        } else {
-                        System.out.println("Peccato! Hai scelto il pezzo sbagliato da assemblare");
-                        }
-                        System.out.println("Inserisci il prossimo pezzo da assemblare: ");
-                    }
-
+                i = 0;
+             while (i == 1) {
+                i = 0;
+                System.out.print("Inserisci il prossimo pezzo da assemblare: ");
+                String componente = input.next();
+                if (componente == "Corde"){
+                System.out.print("Bravo hai scelto il pezzo giusto da assemblare");
+                chitarra.add("Corde");
+                i = 1;
+                } else {
+                System.out.print("Peccato! Hai scelto il pezzo sbagliato da assemblare");
+                }
+                System.out.print("Inserisci il prossimo pezzo da assemblare: ");
+                }
+                    System.out.print("Hai completato la chitarra!");
+                    input.close();
     }
 }
